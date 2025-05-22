6646e7d98a9f07bdd1d6f59435cea2170d55d0a0
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-23 06:37:58 +0700

    .

--- src/apps/listings/models.py
+++ src/apps/listings/models.py
@@ -2,8 +2,7 @@
 from django.db import models
 
 class Listing(models.Model):
-    title = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
 
-    def __str__(self):
-        return self.title
+    class Meta:
+        app_label = 'listings'
+        
